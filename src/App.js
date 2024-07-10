196eeed82e658e3a450d2c11e272291da3b51e3b
server_42 <devbhattacharya42@gmail.com>
2024-07-10 20:33:57 +0530

    Project done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 // import Courses ,Title and UserCard here
 // keep a user object with mentioned properties and pass down to UserCard as prop
+import Title from "./Components/Title";
+import UserCard from "./Components/UserCard";
 export default function App() {
-  return <></>;
+  return (
+    <div>
+      <Title />
+      <UserCard
+        name="Chrisse"
+        avatar="https://upload.wikimedia.org/wikipedia/commons/e/e4/Color-blue.JPG"
+        posts="1841"
+        followers="66868"
+        address="4018 Sachs Trail"
+      />
+    </div>
+  );
 }
--- src/Components/UserCard.jsx
+++ src/Components/UserCard.jsx
@@ -1,2 +1,21 @@
 // Your code goes here
 // do a default export
+import React from "react";
+import styles from "./UserCard.module.css";
+const UserCard = ({ name, avatar, posts, followers, address }) => {
+  return (
+    <div className={styles.container}>
+      <div>
+        <img src={avatar} alt="img" className={styles.avatar} />
+      </div>
+      <h2 data-testid="user_name">{name}</h2>
+      <p data-testid="user_address">{address}</p>
+      <h3>Posts</h3>
+      <p data-testid="user_posts">{posts}</p>
+      <h3>Followers</h3>
+      <p data-testid="user_followers">{followers}</p>
+    </div>
+  );
+};
+
+export default UserCard;
